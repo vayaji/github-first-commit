20811eaaa0cd4fc792754b2cac69b46363515521
vayaji <176355251+vayaji@users.noreply.github.com>
2025-05-23 15:00:53 +0900

    Add animation

--- src/app.tsx
+++ src/app.tsx
@@ -1,15 +1,23 @@
 import { useState } from "preact/hooks";
+import { useReward } from "react-rewards";
 
 export function App() {
   const [url, setUrl] = useState<string>("");
   const [logs, setLogs] = useState<string[]>([]);
   const [firstCommitUrl, setFirstCommitUrl] = useState<string>("");
+  const { reward, isAnimating } = useReward("rewardId", "confetti");
+  const [buttonText, setButtonText] = useState<string>("Get first commit!");
+  const [isSuccess, setIsSuccess] = useState(false);
 
   const appendLog = (log: string) => {
     setLogs((prevLogs) => [...prevLogs, log]);
   };
 
   const handleSubmit = async () => {
+    if (firstCommitUrl) {
+      window.location.href = firstCommitUrl;
+      return;
+    }
     const urlMatch =
       /github\.com\/(?<owner>[^\/]+)\/(?<name>[^\/]+)(?:\/tree\/(?<tree>[^\/]+))?/.exec(
         url,
@@ -24,26 +32,28 @@ export function App() {
     const apiUrl = `https://api.github.com/repos/${owner}/${name}/commits?sha1=${tree}`;
     appendLog(`Fetching ${apiUrl}`);
     const response = await fetch(apiUrl);
-    const linkHeader = response.headers.get("link");
-    if (!linkHeader) {
-      const data = await response.json();
-      appendLog(`Fetched ${data.length} commits`);
-      setFirstCommitUrl(data[data.length - 1].html_url);
-      appendLog("Success!");
-      return;
-    }
 
-    const linkHeaderMatch = /<(?<lastUrl>[^>]+)>; rel="last"/.exec(linkHeader);
-    const lastPage = linkHeaderMatch?.groups?.lastUrl;
-    if (!lastPage) {
-      appendLog("No last page");
-      return;
+    let data = await response.json();
+    const linkHeader = response.headers.get("link");
+    if (linkHeader) {
+      const linkHeaderMatch = /<(?<lastUrl>[^>]+)>; rel="last"/.exec(
+        linkHeader,
+      );
+      const lastPage = linkHeaderMatch?.groups?.lastUrl;
+      if (!lastPage) {
+        appendLog("No last page");
+        return;
+      }
+      const lastPageResponse = await fetch(lastPage);
+      data = await lastPageResponse.json();
     }
-    const lastPageResponse = await fetch(lastPage);
-    const lastPageData = await lastPageResponse.json();
-    appendLog(`Fetched ${lastPageData.length} commits`);
-    setFirstCommitUrl(lastPageData[lastPageData.length - 1].html_url);
+    appendLog(`Fetched ${data.length} commits`);
+    setFirstCommitUrl(data[data.length - 1].html_url);
     appendLog("Success!");
+    reward();
+    setIsSuccess(true);
+    setButtonText("Go to first commit!");
+    return;
   };
 
   return (
@@ -52,9 +62,6 @@ export function App() {
         "flex items-center justify-center h-screen flex-col gap-4 font-outfit"
       }
     >
-      <a href={firstCommitUrl}>
-        <h1>GitHub First Commit</h1>
-      </a>
       <input
         type="text"
         value={url}
@@ -65,12 +72,11 @@ export function App() {
       />
       <button
         type={"button"}
-        className={
-          "p-4 max-w-1/2 w-60 border-2 border-slate-800 rounded-xl text-2xl cursor-pointer"
-        }
+        className={`p-4 max-w-1/2 w-60 border-2 flex flex-col border-slate-800 rounded-xl text-center text-xl cursor-pointer transition-colors duration-500 ${isSuccess ? "bg-gradient-to-r from-[#f27121] via-[#e94057] to-[#8a2387] text-white" : ""}`}
         onClick={handleSubmit}
       >
-        Submit
+        {buttonText}
+        <span id="rewardId" />
       </button>
       <ol className={"flex flex-col text-center"}>
         {logs.map((log, i) => (
